3488e890afdc018bdf1c0d227692a8cfd729f95b
leifeng <2496372828@qq.com>
2022-08-12 11:38:38 +0800

    hot-fix commit first

--- src/main/java/com/sdjtxy/test/GitTest.java
+++ src/main/java/com/sdjtxy/test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("good1");
         System.out.println("good2");
+        System.out.println("hot-fix 分支---");
     }
 }
